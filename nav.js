6ea8605dfe2d0ab6e2c4c50de726eb7562b4bee6
MertcanCinal <mert.cinal@hotmail.com>
2023-07-04 22:59:20 +0300

    Initial commit

---
+++ nav.js
@@ -0,0 +1,19 @@
+const openMenu=() => {
+    document.querySelector(selectors,'.backdrop').classname ='backdrop active';
+    document.querySelector(selectors, 'aside').className = 'active';
+}
+const closeMenu=() => {
+    document.querySelector(selectors,'.backdrop').classname ='backdrop';
+    document.querySelector(selectors, 'aside').className = '';
+}
+
+document.getElementById(elementId,'menuBtn').onclick= e => {
+    e.preventDefault();
+    openMenu();
+}
+document.querySelector(selectors, 'aside button.close').onclick = e => {
+    closeMenu();
+}
+document.querySelector(selectors, '.backdrop').onclick = e => {
+    closeMenu();
+}
